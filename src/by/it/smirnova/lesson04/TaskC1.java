9beb953b08761d062852a0e1ba3f33ae7e1252e5
Maria <ma_s@tut.by>
2018-08-29 08:20:18 +0300

    lesson04 levelb3+c2 ok

--- src/by/it/smirnova/lesson04/TaskC1.java
+++ src/by/it/smirnova/lesson04/TaskC1.java
@@ -38,4 +38,76 @@ import java.util.Scanner;
 За месяц 14 начислено $0.0
 
 */
-//
+public class TaskC1 {
+    public static void main(String[] args) {
+        System.out.println("Какую вы хотите зарплату в $$$?");
+        Scanner sc = new Scanner(System.in);
+        int salary = sc.nextInt();
+
+        if (salary>=300&&salary<=3000) {
+            for (int i = 0; i < 15; i++)
+                switch (i) {
+                    case 0:
+                        System.out.println("За месяц 0 начислено $0.0");
+                        break;
+                    case 1:
+                        System.out.println("За январь начислено $" + salary * 1.5);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 2:
+                        System.out.println("За февраль начислено $" + salary * 1.5);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 3:
+                        System.out.println("За март начислено $" + salary * 1.5);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 4:
+                        System.out.println("За аперль начислено $" + salary * 1.5);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 5:
+                        System.out.println("За май начислено $" + salary * 1.5);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 6:
+                        System.out.println("За июнь начислено $" + salary);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 7:
+                        System.out.println("За июль начислено $" + salary);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 8:
+                        System.out.println("За август начислено $" + salary);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 9:
+                        System.out.println("За сентябрь начислено $" + salary * 1.5);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 10:
+                        System.out.println("За октябрь начислено $" + salary * 1.5);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 11:
+                        System.out.println("За ноябрь начислено $" + salary * 1.5);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 12:
+                        System.out.println("За декабрь начислено $" + salary * 1.5);
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 13:
+                        System.out.println("За месяц 13 начислено $0.0");
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                    case 14:
+                        System.out.println("За месяц 14 начислено $0.0");
+                        if (salary==666||salary*1.5==666){break;}
+                        break;
+                
+
+                }
+        } else System.out.println("Мы вам перезвоним!");}
+}
--- src/by/it/smirnova/lesson04/TaskC2.java
+++ src/by/it/smirnova/lesson04/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.smirnova.lesson04;
 
+import java.util.Scanner;
+
 /*
 
 На входе вводится число и месяц 2018 года.
@@ -27,5 +29,72 @@ package by.it.smirnova.lesson04;
 
  */
 public class TaskC2 {
-
+    public static void main(String[] args) {
+        //int day;
+       // int month;
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        int month = sc.nextInt();
+        //int sum=0;
+        if (day>0&&day<=31){
+            if (month>0&&month<13) {
+                switch (month) {
+                    case 1:
+                        System.out.println(0 + day);
+                        //sum += 31;
+                        break;
+                    case 2:
+                        System.out.println(31 + day);
+                        //sum += 28;//31+28
+                        break;
+                    case 3:
+                        System.out.println(31+28 + day);
+                        //sum += 31;//31+28+31
+                        break;
+                    case 4:
+                        System.out.println(31+28+31 + day);
+                        //sum += 30;//31+28+31+30
+                        break;
+                    case 5:
+                        System.out.println(31+28+31+30 + day);
+                        //sum += 31;//31+28+31+30+31
+                        break;
+                    case 6:
+                        System.out.println(31+28+31+30+31 + day);
+                       // sum += 30;//31+28+31+30+31+30
+                        break;
+                    case 7:
+                        System.out.println(31+28+31+30+31+30 + day);
+                       // sum += 31;//31+28+31+30+31+30+31
+                        break;
+                    case 8:
+                        System.out.println(31+28+31+30+31+30+31 + day);
+                       // sum += 31;//31+28+31+30+31+30+31+31
+                        break;
+                    case 9:
+                        System.out.println(31+28+31+30+31+30+31+31 + day);
+                       // sum += 30;//31+28+31+30+31+30+31+31+30
+                        break;
+                    case 10:
+                        System.out.println(31+28+31+30+31+30+31+31+30 + day);
+                       // sum += 31;//31+28+31+30+31+30+31+31+30+31
+                        break;
+                    case 11:
+                        System.out.println(31+28+31+30+31+30+31+31+30+31 + day);
+                        //sum += 30;//31+28+31+30+31+30+31+31+30+31+30
+                        break;
+                    case 12:
+                        System.out.println(31+28+31+30+31+30+31+31+30+31+30 + day);
+                        //sum += 31;//31+28+31+30+31+30+31+31+30+31+30+31
+                        break;
+                    case 0:
+                        System.out.println(0);
+                        break;
+                    default:
+                        System.out.println(0);
+                }
+            }
+        }
+        else System.out.println(0);
+    }
 }
