46e69ed544b73b9cec83ebab59c46cef72d34f71
Kostya Lyapochkin <kostya500steam@gmail.com>
2018-08-29 12:28:29 +0300

    lesson5 level a2

---
+++ src/by/it/smirnova/lesson05/TaskB1.java
@@ -0,0 +1,23 @@
+package by.it.smirnova.lesson05;
+/*
+Создайте 5 различных строк в списке ArrayList:
+
+1. Создайте список строк.
+2. Добавьте в него 5 различных строк.
+3. Выведите его размер на экран.
+4. Используя цикл выведите его содержимое на экран, каждое значение с новой строки.
+
+*/
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskB1 {
+    //public static void main(String[] args) {
+    //    ArrayList<String> list=new ArrayList<>();
+    //    Scanner scanner=new Scanner(System.in);
+    //    String line = scanner.nextLine();
+     //   list.add(line);
+
+}
