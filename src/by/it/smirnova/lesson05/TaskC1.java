ade0eb9774e9a353e105dcca568764de5a8f1d75
Maria <ma_s@tut.by>
2018-08-30 21:44:09 +0300

    lesson 05 c1

--- src/by/it/smirnova/lesson05/TaskC1.java
+++ src/by/it/smirnova/lesson05/TaskC1.java
@@ -10,14 +10,32 @@ package by.it.smirnova.lesson05;
     Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
 */
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TaskC1 {
     public static void main(String[] args) throws Exception
-    {
+    {   int[] m = new int [20];
+        Scanner sc = new Scanner(System.in);
 
-    }
+        for (int i=0; i < m.length;i++){
+            m[i] = sc.nextInt();
+        }
+        ArrayList list3 = new ArrayList();
+        ArrayList list2 = new ArrayList();
+        ArrayList list = new ArrayList();
 
+        for (int i=0; i < m.length;i++){
+            if (m[i]%3==0) {list3.add(m[i]);}
+            if (m[i]%2==0) {list2.add(m[i]);}
+            if(m[i]%3!=0&&m[i]%2!=0) list.add(m[i]);
+
+            }
+        printList(list3);
+        printList(list2);
+        printList(list);
+        }
     private static void printList(List<Integer> list) {
         for (Integer aList : list) System.out.println(aList);
     }
